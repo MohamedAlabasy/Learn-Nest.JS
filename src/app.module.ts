6e6319208110fffb96be911589a76b82effb43a1
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-26 22:32:28 +0200

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,31 +11,43 @@ import { People } from './typeORM/entities/people.entity';
 import { PeopleModule } from './people/people.module';
 import { Profile } from './typeORM/entities/profile.entity';
 
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     // ItemModule,
     // UsersModule,
     PeopleModule,
+    ConfigModule.forRoot({
+      // load: [configuration],
+      // isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'testss',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       entities: [People, Profile],
       synchronize: true,
       autoLoadEntities: true,
-    }),
-  ],
+    })],
   controllers: [AppController],
   // controllers: [AppController, ItemController],
   providers: [AppService],
   // providers: [AppService, ItemService],
 })
 export class AppModule implements NestModule {
+
   configure(consumer: MiddlewareConsumer) {
+    // console.log('beso');
+    // console.log(process.env.TYPE);
+    // console.log(process.env.HOST);
+    // console.log(process.env.PORT);
+    // console.log(process.env.USER_NAME);
+    // console.log(process.env.PASSWORD);
+    // console.log(process.env.DATABASE);
     // consumer.apply(ExampleMiddleware).forRoutes('*')
 
     // throw new Error('Method not implemented.');
